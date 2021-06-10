1779756eab277fded51e4a2ca1f3e60cdf04c867
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 16:38:08 -0600

    Initial Commit

---
+++ FileManagerDemo/FileManagerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerDemoApp.swift
+//  FileManagerDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
